d58b51894ea0dbfee57fdf04abdd0a16a858f253
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-02-29 12:01:52 +0100

    feat : Refactor login tests and add failure case

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -9,28 +9,37 @@ beforeAll(() => {
 
 test('is_user_logged_in', () => {
     //given
-    let login = new Login(FB.login());
+    let login = new Login(global.FB.login());
     //when
     let status = login.isLoggedIn();
     //then
     expect(status).toBe(true);
 
 })
-test('log_user_in', () => {
+test('log_user_in_success', () => {
     //given
-    let login = new Login(FB.login());
+    let login = new Login(global.FB.login());
     //when
     login.login();
     //then
     expect(login.isLoggedIn()).toBe(true);
 })
-
+test('log_user_in_failure', () => {
+    //given
+    let login = new Login(global.FB.login());
+    //when
+    login.login();
+    //then
+    expect(login.isLoggedIn()).toBe(false);
+})
 test('log_user_out', () => {
     //given
-    let login = new Login(FB.login());
+    let login = new Login(global.FB.login());
     //when
     login.login();
     login.logout();
     //then
     expect(login.isLoggedIn()).toBe(false);
 })
+
+
