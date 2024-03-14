dd9d06512f4a87efbea712a0ddbe75bc98abe460
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-03-14 11:25:33 +0100

    feat : Add login test cases and exceptions

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -2,6 +2,9 @@
 const { before } = require('node:test');
 const Login = require('../Login/login');
 const FB = require('./Login/FB');
+const UnauthorizedLoginException = require('../Login/UnauthorizedLoginException');
+const UnknownLoginException = require('../Login/UnknownLoginException');
+const UndefinedLoginException = require('../Login/UndefinedLoginException');
 
 beforeAll(() => {
     global.FB = new FB();
@@ -12,26 +15,47 @@ test('is_user_logged_in_success', () => {
     let login = new Login();
     login.login(global.FB.login());
     //when
-    let status = login.getLoginStatus(fb.getLoginStatus());
+    let status = login.getLoginStatus(global.FB.getLoginStatus());
     //then
     expect(status).toBe(true);
 
 })
-test('log_user_in_success', () => {
-    //given
-    let login = new Login(global.FB.login());
-    //when
-    login.login(global.FB.login());
-    //then
-    expect(login.getLoginStatus()).toBe(true);
-})
-test('log_user_in_failure', () => {
+test('is_user_logged_in_failure', () => {
     //given
     let login = new Login();
     //when
-    login.login();
+    let status = login.getLoginStatus();
     //then
-    expect(login.getLoginStatus()).toBe(false);
+    expect(status).toBe(false);
+})
+test('log_user_in_success', () => {
+    //given
+    let login = new Login();
+    //when
+    login.login(global.FB.login());
+    //then
+    expect(login.getLoginStatus(global.FB.getLoginStatus())).toBe(true);
+})
+test('log_user_in_not_authorized', () => {
+    //given
+    let login = new Login();
+    //when
+
+    //then
+    expect(() => {
+        login.login(global.FB.loginNotAuthorized());
+    }).toThrowError(UnauthorizedLoginException);
+
+})
+test('log_user_in_unknown', () => {
+    //given
+    let login = new Login();
+    //when
+
+    //then
+    expect(() => {
+        login.login(global.FB.loginFailure());
+    }).toThrowError(UnknownLoginException);
 })
 test('log_user_out_success', () => {
     //given
@@ -40,7 +64,7 @@ test('log_user_out_success', () => {
     login.login();
     login.logout();
     //then
-    expect(login.getLoginStatus()).toBe(false);
+    expect(login.getLoginStatus(global.FB.getLoginStatus())).toBe(false);
 })
 
 
