a7762d0cde7e597277222cb11d43c27321ac35e7
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-03-14 09:14:22 +0100

    refactor : correct bug in test

--- src/test/i18n.test.js
+++ src/test/i18n.test.js
@@ -1,8 +1,7 @@
 const {By, Builder,Select, Browser} = require('selenium-webdriver');
-const assert = require("assert");
 let driver;
 
-describe('i18next language change', function() {
+describe('localisation language change', function() {
     driver = new Builder().forBrowser('chrome').build();
     driver.get('http://localhost:63342/RentABike/public_html/index.html');
     driver.manage().setTimeouts({implicit: 500});
@@ -14,13 +13,13 @@ describe('i18next language change', function() {
 
         let dropdownElement = driver.findElement(By.id('languageSelect'));
         const dropdown = new Select(dropdownElement)
-        driver.manage().setTimeouts({implicit: 500});
 
         // When
         dropdown.selectByVisibleText("DE");
+        driver.manage().setTimeouts({implicit: 500});
 
         // Then
-        expect(userInputElement.value).toEqual(driver.findElement(By.id('userName')).getAttribute('value'));
+        expect(userInputElement.value).toEqual("John Doe");
     });
 
     it('should maintain graphic element positioning', function() {
